38cab5fff3e5d38bcf560bf907cdd1b5cd4f4806
Hrishi19 <47072919+Hrishi19@users.noreply.github.com>
2020-10-02 14:21:47 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        int[] nums=new int[]{ 0, 8, 4, 12, 2};
+       int n=Arrays.binarySearch(nums,0,3,2);
+        System.out.println(n);
+    }
+
+
+}
